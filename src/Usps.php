230d06282b385d6ac8a9b8250a06f330a22a1a2e
Akintilerewa oreoluwa <daveore090@gmail.com>
2021-08-08 00:46:04 +0100

    Update Usps.php

--- src/Usps.php
+++ src/Usps.php
@@ -85,14 +85,14 @@ class Usps {
     public function rate($request){
         $rate = new Rate($this->config['username']);
         $ratepackage = new RatePackage();
-        $ratepackage->setService($request->Service);
-         $ratepackage->setFirstClassMailType($request->FirstClassMailType);
-         $ratepackage->setZipOrigination($request->ZipOrigination);
-         $ratepackage->setZipDestination($request->ZipDestination);
-         $ratepackage->setPounds($request->Pounds);
-         $ratepackage->setOunces($request->Ounces);
-         $ratepackage->setContainer($request->Container);
-         $ratepackage->setSize($request->Size);
+        $ratepackage->setService((array_key_exists('Service', $request) ? $request['Service'] : null );
+         $ratepackage->setFirstClassMailType((array_key_exists('FirstClassMailType', $request) ? $request['FirstClassMailType'] : null ));
+         $ratepackage->setZipOrigination((array_key_exists('ZipOrigination', $request) ? $request['ZipOrigination'] : null ));
+         $ratepackage->setZipDestination($(array_key_exists('ZipDestination', $request) ? $request['ZipDestination'] : null ));
+         $ratepackage->setPounds((array_key_exists('Pounds', $request) ? $request['Pounds'] : null ));
+         $ratepackage->setOunces((array_key_exists('Ounces', $request) ? $request['Ounces'] : null ));
+         $ratepackage->setContainer((array_key_exists('Container', $request) ? $request['Container'] : null ));
+         $ratepackage->setSize((array_key_exists('Size', $request) ? $request['Size'] : null ));
         
         // Add the Package object to the Rate Package class
         $rate->addPackage($ratepackage);
